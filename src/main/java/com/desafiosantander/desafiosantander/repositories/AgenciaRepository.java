19d10eaba9c7b0c9eb02b80c82758ed3aba0b641
diasvictordev <victor.dias@aluno.ueg.br>
2025-09-21 22:49:11 -0300

    add anotacao repository

--- src/main/java/com/desafiosantander/desafiosantander/repositories/AgenciaRepository.java
+++ src/main/java/com/desafiosantander/desafiosantander/repositories/AgenciaRepository.java
@@ -2,6 +2,8 @@ package com.desafiosantander.desafiosantander.repositories;
 
 import com.desafiosantander.desafiosantander.entities.Agencia;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AgenciaRepository extends JpaRepository<Agencia, Long> {
 }
